499de58fe1604974633aa1d8748fe9b3eecf43b6
fahmyeby <fahmyebyyussoff@yahoo.com.sg>
2024-10-21 13:09:29 +0800

    update

--- Permutation1.java
+++ Permutation1.java
@@ -1,17 +1,32 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Permutation1 {
     public static void main(String[] args) {
         String input = "ABCD";
         int length = 4;
 
-        for (int a = 0; a< input.length(); a++) {
-            for (int b = 0; b< input.length(); b++) {
-                for (int c = 0; c< input.length(); c++) {
-                    for (int d = 0; d< input.length(); d++) {
-                        System.out.println(""+input.charAt(a)+input.charAt(b)+input.charAt(c)+input.charAt(d));
+        Set<String> permutations = new HashSet<>();
+
+        for (int a = 0; a < input.length(); a++) {
+            for (int b = 0; b < input.length(); b++) {
+                for (int c = 0; c < input.length(); c++) {
+                    for (int d = 0; d < input.length(); d++) {
+                        System.out.println("" + input.charAt(a) + input.charAt(b) + input.charAt(c) + input.charAt(d));
+
+                        // create permutation string
+                        String permutation = "" + input.charAt(a) + input.charAt(b) + input.charAt(c) + input.charAt(d);
+
+                        // add permutations
+                        permutations.add(permutation);
 
                     }
                 }
             }
+        }
+
+        for (String perm : permutations) {
+                System.out.println(perm);
         }
 
     }
